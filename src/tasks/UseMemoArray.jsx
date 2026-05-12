ff7c17927c08455d3a23bbe4d5edddfb623c11ca
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-05-12 20:53:33 +0500

    feat: solve useMemo task

--- src/tasks/UseMemoArray.jsx
+++ src/tasks/UseMemoArray.jsx
@@ -1,8 +1,19 @@
 import { useState, useMemo } from 'react'
+import { Button } from '@ui/Button'
+
+const generateArray = (array) => array.map(() => Math.floor(Math.random() * 10))
 
 export const UseMemoArray = () => {
+  const [array, setArray] = useState(generateArray([...Array(10)]))
+  const sum = useMemo(() => {
+    return array.reduce((acc, x) => acc + x, 0)
+  }, [array])
+
+
   return (
   <>
-    <span>useMemoArray</span>
+    <span className='mb-4'>Array: {array.join(' ')}</span>
+    <span className='mb-4'>Sum: {sum}</span>
+    <Button onClick={() => setArray(generateArray(array))}>Generate</Button>
   </>)
 }
