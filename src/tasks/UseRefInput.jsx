a4bf4bb8660bfffc2da11e3633fabdfd00472c93
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-05-13 18:16:20 +0500

    feat: solve useRef task

--- src/tasks/UseRefInput.jsx
+++ src/tasks/UseRefInput.jsx
@@ -1,8 +1,40 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { Input } from '@ui/Input'
+import { Button } from '@ui/Button'
 
 export const UseRefInput = () => {
+  const inputRef = useRef(null)
+  const timerRef = useRef(null)
+  const counterRef = useRef(0)
+  const prevInputRef = useRef('')
+  const [input, setInput] = useState('')
+  const [prev, setPrev] = useState('')
+
+  useEffect(() => {
+    counterRef.current++
+    console.log('render count: ', counterRef.current);
+  });
+  
+  const handleSubmit  = () => {
+    setPrev(prevInputRef.current)
+    prevInputRef.current = input
+    inputRef.current?.focus()
+  }
+
+  const handleInput = (e) => {
+    clearTimeout(timerRef.current)
+    timerRef.current = setTimeout(() => {
+      setInput(e.target.value)
+    }, 200)
+  }
   return (
   <>
-    <span>useRefInput</span>
+    <Input className='mb-4'
+      ref={inputRef}
+      onInput={handleInput}
+      value={input}
+    />
+    <span className='mb-4'>Предыдущее значение: {prev}</span>
+    <Button onClick={handleSubmit}>Submit</Button>
   </>)
 }
