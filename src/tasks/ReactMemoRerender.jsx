e79263ec5c8a5b70d54bcef5f30930277dc79976
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-05-15 17:06:17 +0500

    fix: rework solution

--- src/tasks/ReactMemoRerender.jsx
+++ src/tasks/ReactMemoRerender.jsx
@@ -1,7 +1,7 @@
 import { useState, memo, useCallback, useRef } from 'react'
 import { Button } from '@ui/Button'
 
-const ChildComponent = memo(({ children, count, boolean, onClick, good }) => {
+const ChildComponent = memo(({ title, count, boolean, onClick, color }) => {
   const [childCount, setChildCount] = useState(0)
   const renderCount = useRef(0)
 
@@ -9,7 +9,7 @@ const ChildComponent = memo(({ children, count, boolean, onClick, good }) => {
 
   return (
     <div className=' flex flex-col gap-2 py-2 px-4 w-full h-full bg-indigo-200 rounded-xl ring-2 ring-indigo-300'>
-      <span className={good ? 'text-green-600' : 'text-red-600'}><b>{children}</b></span>
+      <span className={color}><b>{title}</b></span>
       <span>Render counter: {renderCount.current}</span>
       {count ? <span>Parent counter: {count ?? 'null'}</span> : null}
       {boolean ? <span>Parent toggle: {boolean + ''}</span> : null}
@@ -17,6 +17,46 @@ const ChildComponent = memo(({ children, count, boolean, onClick, good }) => {
     </div>
   )
 })
+
+const ParentWithJSXNesting = ({ children, className }) => {
+  const [count, setCount] = useState(0)
+
+  const counter = () => {
+    setCount(prev => prev + 1)
+  }
+
+  return (
+    <div className={'flex flex-col gap-2 py-2 px-4 w-full h-full rounded-xl ring-2 ' + className}>
+      <span>Parent with JSX nesting counter: {count}</span>
+      {children}
+      <Button onClick={counter}>
+        Render
+      </Button>
+    </div>
+  );
+}
+
+const ChildWithJSXNesting = ({ children, title, color }) => {
+  const [childCount, setChildCount] = useState(0)
+  const renderCount = useRef(0)
+
+  const counter = () => {
+    setChildCount(prev => prev + 1)
+  }
+
+
+  renderCount.current += 1
+
+  return (
+    <div className=' flex flex-col gap-2 py-2 px-4 w-full h-full bg-indigo-200 rounded-xl ring-2 ring-indigo-300'>
+      <span className={color}><b>{title}</b></span>
+      <span>Render counter: {renderCount.current}</span>
+      {children}
+      <Button onClick={counter}>render</Button>
+    </div>
+  )
+}
+
 
 export const ReactMemoRerender = () => {
   const [count, setCount] = useState(1)
@@ -33,24 +73,17 @@ export const ReactMemoRerender = () => {
   return (
   <div className='flex flex-col gap-4 w-full min-w-60 h-full bg-indigo-100 p-4 rounded-2xl'>
     <span>Parent counter: {count}</span>
-    <ChildComponent count={count}>
-      Child with changed parent state
-    </ChildComponent>
-    <ChildComponent good={true}>
-      Child without changed parent state
-    </ChildComponent>
-    <ChildComponent value={{}}>
-      Child with new object prop
-    </ChildComponent>
-    <ChildComponent boolean={boolean} onClick={toggle} good={true}>
-      Child with cached function
-    </ChildComponent>
-    <ChildComponent boolean={boolean} onClick={toggle2}>
-      Child without cached function
-    </ChildComponent>
     <Button onClick={() => setCount(prev => prev + 1)}>
       Render
     </Button>
+    <ChildComponent count={count} title='Child with changed parent state' color='text-red-600'/>
+    <ChildComponent title='Child without changed parent state' color='text-green-600'/>
+    <ChildComponent value={{}} title='Child with new object prop' color='text-red-600'/>
+    <ChildComponent boolean={boolean} onClick={toggle} good title='Child with cached function' color='text-green-600'/>
+    <ChildComponent boolean={boolean} onClick={toggle2} title='Child without cached function' color='text-red-600'/>
+    <ParentWithJSXNesting className='mt-6'>
+      <ChildWithJSXNesting title='Child with JSX nesting' color='text-green-600'/>
+    </ParentWithJSXNesting>
   </div>
   )
 }
