b7f6d005d782d353bd830c19e39d60866d293489
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-05-15 10:32:39 +0500

    fix: rework solution

--- src/tasks/UseMemoArray.jsx
+++ src/tasks/UseMemoArray.jsx
@@ -1,19 +1,33 @@
 import { useState, useMemo } from 'react'
 import { Button } from '@ui/Button'
 
-const generateArray = (array) => array.map(() => Math.floor(Math.random() * 10))
+const generateArray = (num) => Array.from({ length: num}, (() => Math.floor(Math.random() * 10) + 1))
+let cachedArray = generateArray(10000000)
+
+const FieldComponent = ({children, className}) => {
+  return (
+    <span className={className}>{children}</span>
+  )
+}
 
 export const UseMemoArray = () => {
-  const [array, setArray] = useState(generateArray([...Array(10)]))
+  const [array, setArray] = useState(cachedArray)
+  const [count, setCount] = useState(0)
   const sum = useMemo(() => {
-    return array.reduce((acc, x) => acc + x, 0)
+    console.log('calculate');
+
+    let sum = 0
+    for (let num of array) {
+      sum += num
+    }
+    return sum
   }, [array])
 
 
   return (
-  <>
-    <span className='mb-4'>Array: {array.join(' ')}</span>
-    <span className='mb-4'>Sum: {sum}</span>
-    <Button onClick={() => setArray(generateArray(array))}>Generate</Button>
-  </>)
+  <div className='flex flex-col gap-2 py-2 px-4 w-full h-full min-w-60 bg-indigo-200 rounded-xl ring-2 ring-indigo-300'>
+    <FieldComponent className='mb-4'>Sum: {sum}</FieldComponent>
+    <Button onClick={() => setArray(generateArray(10000000))}>generate</Button>
+    <Button onClick={() => setCount(prev => prev + 1)}>render</Button>
+  </div>)
 }
